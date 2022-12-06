876a16ec9d130e7bc212d80c831b8868f8dc61a2
kendrick Brown <chefken360@gmail.com>
2022-12-06 15:47:51 -0800

    manifesto

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,30 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
 const constantDb = await openDB ('jate',1);
 const trx = constantDb.transaction('jate', 'readwrite');
 const store = trx.objectStore('jate');
 const req = store.put(content);
 const res = await req;
 console.log('Saved Data', res);
-return result;
+return result;}
 
 
 // const result = await request;
-console.log('Saved Data', result);
-return result;
+// console.log('Saved Data', result);
+// return result;
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
 const jateDb = await openDB ('jate',1);
 const tx = jateDb.transaction('jate', 'readwrite');
 const stor = tx.objectStore('jate');
 const request = stor.getAll();
 const result = await request;
-console.log('Saved Data', result);
-return result;
+result.length > 0
+    ? console.log('Data retrieved from database')
+    : console.log('Data not found in the database.');
+
+return result?.values() || [];}
+
 
 
 initdb();
