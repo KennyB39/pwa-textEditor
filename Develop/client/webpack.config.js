7f3311962958bbbee3e831a64ad12a07595138a4
kendrick Brown <chefken360@gmail.com>
2022-12-07 04:04:48 -0800

    webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,61 +1,48 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-
+const {injectManifest} = require('workbox-webpack-plugin');
 
 module.exports = () => {
-  return {
+  return{
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
+      main: './src/index.js',
+      install: './src/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js'
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'TODOs List'
+      new HTMLWebpackPlugin({
+        template: './src/index.html',
+        title: 'PWA Demo',
+        chunks: ['main']
       }),
-
+      new injectManifest({
+        swsrc: './src/sw.js',
+        swDest: 'sw.js'
+      }),
       new WebpackPwaManifest({
-        name: 'TODOs',
-        short_name: 'TODOs',
-        description: 'Keep track of important tasks!',
-        background_color: '#7eb4e2',
-        theme_color: '#7eb4e2',
-        start_url: './',
-
+        fingerprints: false,
+        inject:true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor',
+        background_color: '##225ca3',
+        theme_color: '##225ca3',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join( 'src', 'images'),
-          },
-        ],
-      }),
-     
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join('assets', 'icons'),
+          }  
+        ]
+      })
     ],
 
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
-          },
-        },
-      ],
-    },
-  };
-};
+  }
+}
